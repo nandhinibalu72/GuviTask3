3e3d8e82665a012b4e697e21158276d8261876f7
nandhinibalu72 <nandhinibalu72@gmail.com>
2025-01-18 16:12:25 +0530

    Task 3 programs code

---
+++ guess_the_number.py
@@ -0,0 +1,19 @@
+#importing random inbuilt library
+import random
+print("Let's play a number guesssing game from the range of 20 to 40. Let's start")
+ #intializing random range of numbers from 20 to 30
+random_number_to_guess=random.randint(20,40)
+ #initializing while to verify the user guess match with random number generated.
+while True:
+     #get the input from user to guess the value
+     guess_of_user=int(input("Enter a number to guess the value: "))
+     #Verifying if user guess is less than random number printing Too low as output
+     if guess_of_user < random_number_to_guess: 
+         print("Too low, try again")
+     #Verifying if user guess is greater than random number printing Too high as output
+     elif guess_of_user > random_number_to_guess:
+         print("Too high, Try again")
+     #Verifying if user guess is match with random number printing as Correct guess output
+     else:
+         print(f"Correct guess, The number is {random_number_to_guess} congratulations")
+         break
---
+++ scrambled_word.py
@@ -0,0 +1,18 @@
+import random
+print("Welcome to the scrambled word game. Let's start")
+ #initializing the words in list
+words=['python','javascript','java','automation','pytest','guvi','selenium']
+ # picking up a random word to scramble 
+original_word= random.choice(words) 
+ #scrambling the selected word using random sample
+scrambled_word=''.join(random.sample(original_word,len(original_word))) 
+print(f"scrambled_word :{scrambled_word}")
+ # Game loop 
+while True:
+     user_guess=input("Guess the word: ").lower()
+     #verifying user input matches with original word. 
+     if user_guess==original_word:
+         print("congratulations, you guessed it ")
+         break
+     else:
+         print("Wrong guess, Try again!")
